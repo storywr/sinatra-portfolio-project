0a0cbc26d5a6491ee398726217f5376512c973f7
William R. Story <storywr@gmail.com>
2016-12-15 02:40:51 -0500

    create teams

---
+++ db/migrate/20161215073906_create_teams.rb
@@ -0,0 +1,13 @@
+class CreateTeams < ActiveRecord::Migration[5.0]
+  def change
+    create_table :teams do |t|
+      t.string :qb
+      t.string :rb
+      t.string :wr
+      t.string :te
+      t.string :defense
+      t.string :kicker
+      t.integer :user_id
+    end
+  end
+end
